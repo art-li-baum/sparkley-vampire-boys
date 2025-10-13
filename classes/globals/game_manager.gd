33ff35f163a26fb5a3a319f7fdd40706f9a0e209
Arthur Li Baum <art.li.baum@gmail.com>
2025-10-13 14:57:38 +0100

    these files somehow got missed in the commit :(

--- classes/globals/game_manager.gd
+++ classes/globals/game_manager.gd
@@ -2,10 +2,14 @@
 extends Node
 
 signal newBlogPost(bp : BlogPost)
+signal completedPost()
+signal postFailed()
 
 var _sequence : Sequence = preload("uid://b6xhx4uhti62q").instantiate()
 
 var current_loop : int = 0 
+
+var loop_scores : Array[int] = []
 
 enum STATE
 {
@@ -16,6 +20,16 @@ enum STATE
 }
 
 var current_state : STATE = STATE.INITIAL
+
+func post_score(score : int):
+	if(loop_scores.size()-1 < current_loop):
+		loop_scores.push_back(score)
+	else:
+		print("trying to add a score for a round that exists :(")
+	
+	print("earned score %s for the %s round" % [score,current_loop]) 
+	
+	current_state = STATE.EVAL
 
 func _process(delta: float) -> void:
 	if(current_state == STATE.INITIAL):
--- classes/post_container.gd
+++ classes/post_container.gd
@@ -11,10 +11,11 @@ var eval_list : Array[int]
 var current_index = 0
 
 const grey_hex = "#6F7378"
-const white_hex = "#ffffff"
+const white_hex = "#f8f8ff"
 
 func _ready() -> void: 
 	GameManager.newBlogPost.connect(_pull_post)
+	GameManager.postFailed.connect(evaluate_post_progress)
 
 func _pull_post(post : BlogPost):
 	#Destroy all childern
@@ -30,11 +31,26 @@ func _pull_post(post : BlogPost):
 		inst.text = "[color=%s]%s[/color]" % [grey_hex , line]
 		line_textboxes.push_back(inst)
 		add_child(inst)
-	
-	#TODO: Set post timer
-	
-	#TODO: load first line for eval
+		
+	#load first line for eval
 	_load_line(0)
+
+
+func _input(event: InputEvent) -> void:
+	if(GameManager.current_state != GameManager.STATE.POST): return
+	
+	if(event is InputEventKey and event.pressed and event.echo == false): 
+		var unicode: int
+		if not event.shift_pressed:
+			unicode = event.keycode | 0x20
+		else:
+			unicode = event.keycode
+		if unicode > 20 and unicode < 40_000: # filters most control characters
+			var letter := String.chr(unicode)
+			if(check_input(letter)):
+				update_line_visual()
+				check_if_done()
+
 
 func _load_line(line_index : int):
 	current_line = line_index
@@ -67,7 +83,6 @@ func check_input(char : String) ->bool :
 	
 	var line = line_list[current_line]
 
-	
 	#check if the current index is the character
 	if(line[current_index] == char):
 		eval_list[current_index] = 1
@@ -94,7 +109,6 @@ func check_input(char : String) ->bool :
 	#if there was no match
 	return false
 
-
 func check_if_done():
 	
 	while(eval_list[current_index] > 0):
@@ -103,10 +117,17 @@ func check_if_done():
 		if(current_index >= current_line_end):
 			#TEST
 			print("finished the line")
+			current_line += 1
+			if(current_line < line_list.size()):
+				_load_line(current_line)
+			else:
+				#send signal that post was complete
+				#TEST
+				print("completed post")
+				GameManager.completedPost.emit()
 			return
 		
 	pass
-
 
 func update_line_visual():
 	#build the visual line from scratch, we'll see if this is performant :/
@@ -130,15 +151,11 @@ func update_line_visual():
 	
 	box.text = t
 
-func _input(event: InputEvent) -> void:
-	if(event is InputEventKey and event.pressed and event.echo == false): 
-		var unicode: int
-		if not event.shift_pressed:
-			unicode = event.keycode | 0x20
-		else:
-			unicode = event.keycode
-		if unicode > 20 and unicode < 40_000: # filters most control characters
-			var letter := String.chr(unicode)
-			if(check_input(letter)):
-				update_line_visual()
-				check_if_done()
+func evaluate_post_progress(finished : bool = false):
+	if(finished):
+		GameManager.post_score(100)
+		return
+		
+	#TODO:evaluate score based on percentage of lines completed weighted by word count?
+	var score = float(current_line)/line_list.size() * 100
+	GameManager.post_score(score)
