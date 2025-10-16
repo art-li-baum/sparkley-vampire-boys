f208b69907db166628da1acf647a58e0ee32d3f7
Arthur Li Baum <art.li.baum@gmail.com>
2025-10-16 15:15:45 +0100

    cleaned up the comment posts a little

--- classes/comment_containter.gd
+++ classes/comment_containter.gd
@@ -6,16 +6,18 @@ var comment_scene : PackedScene = preload("uid://5bhsi4neomru")
 
 func _ready() -> void:
 	GameManager.openEval.connect(get_eval)
+	GameManager.newBlogPost.connect(_clear_comments)
 	
+	_clear_comments()
+
+func _clear_comments(_bp = null):
 	#Destroy all childern initally
 	for n in get_children():
 		n.queue_free()
 
 
 func get_eval(loop : int, score : int):
-	#Destroy all childern
-	for n in get_children():
-		n.queue_free()
+	_clear_comments()
 	
 	#TEST choose 2 - 4 comments per post
 	var num_comments = randi() % 3 + 2
@@ -31,6 +33,8 @@ func get_eval(loop : int, score : int):
 		var content : Comment = u.pull_comment(loop,score)
 		if(content == null): print("NO VALID COMMENT")
 		c.comment_box.text = content.comment_string
+		
+		await get_tree().create_timer(randf() * .75).timeout
 		
 		add_child(c)
 		
