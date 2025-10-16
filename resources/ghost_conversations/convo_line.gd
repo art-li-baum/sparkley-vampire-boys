46b4ab638a0bcfbc324c9a012c8b66234627543c
sunflorebell <108166573+sunflorebell@users.noreply.github.com>
2025-10-16 02:27:14 -0500

    a bit of introductory text, a couple UI tweaks

--- resources/ghost_conversations/convo_line.gd
+++ resources/ghost_conversations/convo_line.gd
@@ -1,5 +1,5 @@
 class_name ConvoLine extends Resource
 
-@export var line_text : String = "default"
+@export_multiline var line_text : String = "default"
 @export var anim_state : String = "idle_float"
 #TODO text read speed?
